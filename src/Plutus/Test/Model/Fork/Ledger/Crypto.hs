6ba752f559c511a19de53a6ae42f51d8848b81bc
Kirill Boltaev <aske@fmap.me>
2022-07-05 23:03:23 +0300

    Hide PubKey, PrivateKey from Plutus.Crypto import

--- src/Plutus/Test/Model/Fork/Ledger/Crypto.hs
+++ src/Plutus/Test/Model/Fork/Ledger/Crypto.hs
@@ -44,7 +44,7 @@ import Data.String
 import GHC.Generics (Generic)
 import Plutus.V1.Ledger.Api (LedgerBytes (LedgerBytes), TxId (TxId), fromBuiltin, toBuiltin, fromBytes)
 import Plutus.V1.Ledger.Bytes qualified as KB
-import Plutus.V1.Ledger.Crypto as Export hiding (Signature)
+import Plutus.V1.Ledger.Crypto as Export hiding (Signature, getSignature, PubKey, getPubKey, PrivateKey, getPrivateKey)
 import PlutusTx qualified
 import PlutusTx.Lift (makeLift)
 import PlutusTx.Prelude qualified as PlutusTx
